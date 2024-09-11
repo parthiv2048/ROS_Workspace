c13c838bbdb263078ca92f286cba499cdc2735fe
parthiv2048 <parthiv2048@gmail.com>
2024-09-11 13:52:53 +0000

    Finished my_odom, now working on basic_mover

--- src/basic_mover.py
+++ src/basic_mover.py
@@ -11,20 +11,53 @@ class BasicMover:
         self.my_odom_sub = rospy.Subscriber('my_odom', Point, self.my_odom_cb)
         # Current heading of the robot.
         self.cur_yaw = None
+        self.cur_dist = None
 
     def my_odom_cb(self, msg):
         """Callback function for `self.my_odom_sub`."""
-        raise NotImplementedError
+        self.cur_yaw = msg.z
+        self.cur_dist = msg.x
 
     def turn_to_heading(self, target_yaw):
         """
         Turns the robot to heading `target_yaw`.
         """
-        raise NotImplementedError
+        while self.cur_yaw is None:
+            pass
+        
+        twist = Twist()
+        rate = rospy.Rate(10)
+
+        target_rad = target_yaw * math.pi / 180
+
+        while not rospy.is_shutdown():
+            if math.isclose(target_rad, self.cur_yaw):
+                break
+            twist.angular.z = 0.5 * (target_rad - self.cur_yaw)
+            self.cmd_vel_pub.publish(twist)
+            rate.sleep()
         
     def move_forward(self, target_dist):
         """Moves the robot forward by `target_dist`."""
-        raise NotImplementedError
+        while self.cur_dist is None:
+            pass
+
+        twist = Twist()
+        rate = rospy.Rate(10)
+        initial_dist = self.cur_dist
+        # final_dist = initial_dist + target_dist
+        max_speed = 0.2
+
+        while not rospy.is_shutdown():
+            dist_trav = self.cur_dist - initial_dist
+            if dist_trav < (target_dist / 2.0):
+                twist.linear.x = max_speed
+            elif dist_trav > (target_dist / 2.0):
+                twist.linear.x = (target_dist - dist_trav) * max_speed
+            if dist_trav >= target_dist:
+                break
+            self.cmd_vel_pub.publish(twist)
+            rate.sleep()
 
     def out_and_back(self, target_dist):
         """
@@ -33,7 +66,9 @@ class BasicMover:
         2. turns the robot by 180 degrees; and
         3. moves the robot forward by `target_dist`.
         """
-        raise NotImplementedError
+        self.move_forward(target_dist)
+        self.turn_to_heading(180)
+        self.move_forward(target_dist)
 
     def draw_square(self, side_length):
         """
@@ -58,6 +93,8 @@ class BasicMover:
 
 if __name__ == '__main__':
     rospy.init_node('basic_mover')
+    # BasicMover().turn_to_heading(90)
+    # BasicMover().move_forward(3)
     BasicMover().out_and_back(1)
     # BasicMover().draw_square(1)
     # BasicMover().move_in_a_circle(1)
--- src/my_odom.py
+++ src/my_odom.py
@@ -8,20 +8,22 @@ from tf.transformations import euler_from_quaternion
 
 class MyOdom:
     def __init__(self):
-        self.odom_sub = rospy.Subscriber('odom', Odometry, self.odom_cb)
-        self.my_odom_pub = rospy.Publisher('my_odom', Point, queue_size=1)
         data_odom = None
         while data_odom is None:
             try:
-                data_odom = rospy.wait_for_message()
-        self.old_pose = None 
+                data_odom = rospy.wait_for_message('/odom', Odometry, timeout=1)
+            except:
+                rospy.loginfo("Current odom not ready yet, retrying for setting up init pose")
+        self.old_pose = data_odom.pose.pose
+        self.odom_sub = rospy.Subscriber('odom', Odometry, self.odom_cb)
+        self.my_odom_pub = rospy.Publisher('my_odom', Point, queue_size=1)
         self.dist = 0.0
         self.yaw = 0.0
                 
     def odom_cb(self, msg):
         """Callback function for `odom_sub`."""
         cur_pose = msg.pose.pose
-        self.update_dist(cur_pose) 
+        self.update_dist(cur_pose)
         self.update_yaw(cur_pose.orientation)
         self.publish_data()
 
@@ -31,14 +33,16 @@ class MyOdom:
         Updates `self.dist` to the distance between `self.old_pose` and
         `cur_pose`.
         """
-        raise NotImplementedError
+        self.dist = cur_pose.position.x - self.old_pose.position.x
 
     def update_yaw(self, cur_orientation):
         """
         Helper to `odom_cb`.
         Updates `self.yaw` to current heading of robot.
         """
-        raise NotImplementedError
+        orientation_list = [cur_orientation.x, cur_orientation.y, cur_orientation.z, cur_orientation.w]
+        (cur_roll, cur_pitch, cur_yaw) = euler_from_quaternion(orientation_list)
+        self.yaw = cur_yaw
 
     def publish_data(self):
         """
@@ -46,7 +50,10 @@ class MyOdom:
         """
         # The `Point` object should be used simply as a data container for
         # `self.dist` and `self.yaw` so we can publish it on `my_odom`.
-        raise NotImplementedError
+        cur_point = Point()
+        cur_point.x = self.dist
+        cur_point.z = self.yaw
+        self.my_odom_pub.publish(cur_point)
         
 if __name__ == '__main__':
     rospy.init_node('my_odom')
