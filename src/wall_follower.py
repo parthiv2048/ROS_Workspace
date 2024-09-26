7a28f1d062b5cef2c1c515891ff6e50eb515ceb2
parthiv2048 <parthiv2048@gmail.com>
2024-09-26 18:26:19 +0000

    Use zones instead of angles

--- src/wall_follower.py
+++ src/wall_follower.py
@@ -4,28 +4,26 @@ import rospy
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 
+# Max linear speed the robot is allowed to reach
 MAX_SPEED = 0.3
+# Distance to be maintained from wall
 DIST_FROM_WALL = 1
+# Proportional constant for distance error
+KP = 1.5
+# Derivative constant for distance error
+KD = 0.5
+# Proportional constant for angular error
+KPA = 1
 
 class WallFollower:
     def __init__(self):
         self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
         self.scan_sub = rospy.Subscriber('/scan', LaserScan, self.scan_cb)
+        # Linear Velocity
         self.v = 0
+        # Angular Velocity
         self.w = 0
-        # Proportional constant for distance error
-        self.kp = 2
-        # Derivative constant for distance error
-        self.kd = 0.5
-        # Proportional constant for angular error
-        self.pa = 1
         self.prev_dist_error = 0
-    
-    def filter_range(self, ranges, range_min, range_max):
-        filtered_range = [r for r in ranges if not(math.isnan(r)) and not(math.isinf(r)) and r > range_min and r < range_max]
-        if len(filtered_range) == 0:
-            return [0]
-        return filtered_range
 
     def scan_cb(self, msg):
         """
@@ -46,28 +44,47 @@ class WallFollower:
                 self.v = 0.6 * MAX_SPEED
 
         # Scan all distances on the right side of the robot
-        shortest_dist_angle = 180
-        for angle in range(180, 360):
-            current_dist = msg.ranges[angle]
-            if math.isnan(current_dist) or math.isinf(current_dist) or current_dist < msg.range_min or current_dist > msg.range_max:
-                continue
-            # Find the angle with the lowest distance
-            if current_dist < msg.ranges[shortest_dist_angle]:
-                shortest_dist_angle = angle
+        # The right side is divided into zones, each with the same zone_size
+        # For example - the 181 degree zone covers 179 to 183 degrees given a zone_size of 4
+        # In total, given a zone_size of 4, there will be 44 zones measured ranging from 181 degrees to 357 degrees
+        shortest_dist_angle = 181
+        zone_size = 4
+        current_angle = 181
+        while current_angle < 360:
+            # current_dist stores the average of distances in the zone
+            current_dist = 0
+            # Number of valid angles in the zone
+            angles_counted = 0
+            for angle in range(current_angle - int(zone_size/2), current_angle + int(zone_size/2)):
+                if angle > 359:
+                    break
+                # Skip if distance at this angle is invalid
+                if math.isnan(msg.ranges[angle]) or math.isinf(msg.ranges[angle]) or msg.ranges[angle] < msg.range_min or msg.ranges[angle] > msg.range_max:
+                    continue
+                # Calculate the sum of distances in each zone
+                current_dist += msg.ranges[angle]
+                angles_counted += 1
+            if angles_counted != 0:
+                # Find average distance in the zone
+                current_dist /= angles_counted
+                # Find the zone angle with the lowest average distance
+                if current_dist < msg.ranges[shortest_dist_angle]:
+                    shortest_dist_angle = current_angle
+            current_angle += zone_size
+        
         # Find the lowest distance
         shortest_dist = msg.ranges[shortest_dist_angle]
-        # Convert angle to radians
+        # Convert zone angle to radians
         shortest_dist_angle_rad = shortest_dist_angle * math.pi / 180
         # Shift angle by -2pi because ROS uses angles from pi to -pi
         shortest_dist_angle_rad -= 2 * math.pi
-        print("shortest_dist_angle_rad:", shortest_dist_angle_rad, "shortest_dist:", shortest_dist)
         # Calculate distance error, derivative of distance error, and angular error needed for PD controller
         dist_error = DIST_FROM_WALL - shortest_dist
         dist_derivative = dist_error - self.prev_dist_error
         angular_error = shortest_dist_angle_rad - ((-math.pi)/2)
         self.prev_dist_error = dist_error
         # PD controller using both distance and angle error
-        self.w = self.kp * dist_error + self.kd * dist_derivative + self.pa * angular_error
+        self.w = KP * dist_error + KD * dist_derivative + KPA * angular_error
 
     def follow_wall(self):
         """
